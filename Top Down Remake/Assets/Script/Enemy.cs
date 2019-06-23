5741f14f94e0228817a517509961d7561db2ec17
sheido <af8081@gmail.com>
2019-06-23 20:13:59 +0200

    editing the collision with player and enemies & room type

--- Top Down Remake/Assets/Script/Enemy.cs
+++ Top Down Remake/Assets/Script/Enemy.cs
@@ -22,7 +22,7 @@ public class Enemy : MonoBehaviour
         HealthSystem = this.gameObject.AddComponent<HealthSystem>();
     }
 
-    void OnCollisionEnter2D(Collision2D Collision)
+    void OnCollisionStay2D(Collision2D Collision)
     {
         // projectile damages
         if (Collision.gameObject.tag == Tags.Projectile.ToString())
--- Top Down Remake/Assets/Script/PlayerController.cs
+++ Top Down Remake/Assets/Script/PlayerController.cs
@@ -5,16 +5,16 @@ using Zenject;
 
 public class PlayerController : MonoBehaviour
 {
-	[Inject] private GameSettings _gameSettings;
+    [Inject] private GameSettings _gameSettings;
     [Inject] private PlayerView _playerView;
 
-	public Rigidbody2D body;
+    public Rigidbody2D body;
 
-	private float horizontal;
-	private float vertical;
-	private float moveLimiter;
-	private float runSpeedHorizontal;
-	private float runSpeedVectical;
+    private float horizontal;
+    private float vertical;
+    private float moveLimiter;
+    private float runSpeedHorizontal;
+    private float runSpeedVectical;
 
     private GameManager _gameManager;
     private HealthSystem _healthSystem;
@@ -24,41 +24,43 @@ public class PlayerController : MonoBehaviour
     public void SetupGameManager(GameManager gameManager)
     {
         _gameManager = gameManager;
+    }
+
+    void Start()
+    {
+        this.gameObject.tag = Tags.Player.ToString();
+
+        _initialPosition = this.transform.position;
+
+        moveLimiter = _gameSettings.PlayerDiagonalSpeedLimiter;
+        runSpeedHorizontal = _gameSettings.PlayerRunSpeedHorizontal;
+        runSpeedVectical = _gameSettings.PlayerRunSpeedVertical;
 
         _healthSystem = this.gameObject.AddComponent<HealthSystem>();
 
         _healthSystem.Setup(_gameSettings.PlayerMaxHealth, _gameSettings);
+        Debug.Log("Player health is now = " + _healthSystem.GetCurrentHealth());
     }
 
-    void Start()
-	{
-        _initialPosition = this.transform.position;
+    void Update()
+    {
+        // Gives a value between -1 and 1
+        horizontal = Input.GetAxisRaw(_gameSettings.InputAxisHorizontalName); // -1 is left
+        vertical = Input.GetAxisRaw(_gameSettings.InputAxisVerticalName); // -1 is down
+    }
 
-        moveLimiter = _gameSettings.PlayerDiagonalSpeedLimiter;
-		runSpeedHorizontal = _gameSettings.PlayerRunSpeedHorizontal;
-		runSpeedVectical = _gameSettings.PlayerRunSpeedVertical;
-	}
+    void FixedUpdate()
+    {
+        if (horizontal != 0 && vertical != 0) // Check for diagonal movement
+        {
+            // limit movement speed diagonally, so you move at 70% speed
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
 
-	void Update()
-	{
-		// Gives a value between -1 and 1
-		horizontal = Input.GetAxisRaw(_gameSettings.InputAxisHorizontalName); // -1 is left
-		vertical = Input.GetAxisRaw(_gameSettings.InputAxisVerticalName); // -1 is down
-	}
-
-	void FixedUpdate()
-	{
-		if (horizontal != 0 && vertical != 0) // Check for diagonal movement
-		{
-			// limit movement speed diagonally, so you move at 70% speed
-			horizontal *= moveLimiter;
-			vertical *= moveLimiter;
-		}
-
-		body.velocity = new Vector2(horizontal * runSpeedHorizontal, vertical * runSpeedVectical);
+        body.velocity = new Vector2(horizontal * runSpeedHorizontal, vertical * runSpeedVectical);
 
         UpdateView();
-
     }
 
     void OnTriggerEnter2D(Collider2D collider)
@@ -69,6 +71,37 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == Tags.Enemy.ToString())
+        {
+            TakeDamage(_gameSettings.EnemyToPlayerCollisionDamage);
+        }
+
+        if (collision.gameObject.tag == Tags.Enemy.ToString())
+        {
+            TakeDamage(_gameSettings.BossToPlayerCollisionDamage);
+        }
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        if (!_healthSystem.CanTakeDamageAgain)
+        {
+            _healthSystem.ModifyHealth(damageAmount);
+
+            if (_healthSystem.GetCurrentHealth() <= 0)
+            {
+                //Game Over Player
+                Debug.Log("Can't go lower, guess i'll die");
+            }
+        }
+        else
+        {
+            Debug.Log("Invincible, Monsters !");
+        }
+    }
+
     public void RePositionPlayer()
     {
         this.transform.position = _initialPosition;
@@ -76,6 +109,6 @@ public class PlayerController : MonoBehaviour
 
     void UpdateView()
     {
-        _playerView.PlayerLifeView.fillAmount = _healthSystem.GetCurrentHealth() / _gameSettings.PlayerMaxHealth;
+        _playerView.PlayerLifeView.fillAmount = (float)_healthSystem.GetCurrentHealth() / (float)_gameSettings.PlayerMaxHealth;
     }
 }
